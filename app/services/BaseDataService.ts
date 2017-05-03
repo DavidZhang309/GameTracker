85b03c935cb140d55c7766c457875a6e6f70dd05
DavidZhang309 <david_zhang_309@hotmail.com>
2017-05-03 13:27:43 -0400

    Added BaseDataService

---
+++ app/services/BaseDataService.ts
@@ -0,0 +1,27 @@
+import * as http from 'https';
+import * as https from 'https';
+
+export abstract class BaseDataService {
+    public abstract getAPIHost();
+
+    /**
+     * Queries the specified API using args.
+     * 
+     * @param api The API path
+     * @param args The query arguments
+     * @param secure Is the api secure
+     */
+    public queryAPI(path, secure: boolean): Promise<string> {
+        let getFn = secure ? https.get : http.get;
+        return new Promise<string>((resolve, reject) => {
+            getFn({
+                host: this.getAPIHost(),
+                path: path
+            }, (response) => {
+                let chunks = [];
+                response.on('data', (chunk) => { chunks.push(chunk); });
+                response.on('end', () => { resolve(chunks.join('')); })
+            });
+        });
+    }
+}
