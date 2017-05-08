8578011e9df63224db151073c03e4f02d5767e8d
DavidZhang309 <david_zhang_309@hotmail.com>
2017-05-08 16:18:45 -0400

    Added get api for tracker items

--- app/middleware/TrackerRouter.ts
+++ app/middleware/TrackerRouter.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import { TrackerDataService } from '../services/TrackerDataService';
 import { IServiceRouter } from './IServiceRouter';
 
@@ -7,6 +7,30 @@ export class TrackerRouter implements IServiceRouter {
     router = Router();
 
     public constructor() {
-        
+        this.router.get('/api/item', (request, response) => { this.apiGetItem(request, response); });
+        //this.router.put('/api/item/:item', (request, response) => { this.apiAddItem(request, response); });
     }
+
+    protected apiGetItem(request: Request, response: Response) {
+        try {
+            let filter = { };
+            if (request.param('id') != null) {
+                filter['item_id'] = parseInt(request.param('id'));
+            } else if (request.param('search') != null) {
+                filter['name'] = { $regex: request.param('search') };
+            }
+
+            this.service.getItem(filter).then((docs) => {
+                response.send(docs);
+            }).catch((err) => {
+                response.send({ error: 'unknown error' });    
+            })  
+        } catch (err) {
+            response.send({ error: 'bad arguments' });
+        }
+    }
+
+    // protected apiAddItem(request: Request, response: Response) {
+
+    // }
 }
