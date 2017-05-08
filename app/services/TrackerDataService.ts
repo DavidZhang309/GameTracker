27dfa629ce6d0280ad27d1fcbcd9db51acac5d32
DavidZhang309 <david_zhang_309@hotmail.com>
2017-05-08 16:18:21 -0400

    Added get/update db operations for tracker items

--- app/services/TrackerDataService.ts
+++ app/services/TrackerDataService.ts
@@ -1,9 +1,31 @@
 import { BaseDataService } from './BaseDataService';
+import { MongoClient, Db } from 'mongodb';
 
 export class TrackerDataService extends BaseDataService {
     public getAPIHost() {
         return null;
     }
 
-    
+    public getItem(filter, db: Db = null): Promise<any[]> {
+        if (db == null) {
+            return this.connectToDB().then((db: Db) => {
+                return this.getItem(filter, db);
+            });
+        } else {
+            return db.collection('tracker_items').find(filter).toArray();
+        }
+    }
+
+    public updateItem(itemData, db: Db = null) {
+        if (db == null) {
+            return this.connectToDB().then((db: Db) => {
+                return this.updateItem(itemData, db);
+            });
+        } else {
+            return db.collection('tracker_items').updateOne(
+                { item_id: itemData.item_id }, 
+                itemData
+            );
+        }
+    }
 }
