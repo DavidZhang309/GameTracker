a29618db457e88647c86f5ed9bb5fc76599b4680
DavidZhang309 <david_zhang_309@hotmail.com>
2017-05-09 13:41:34 -0400

    Updated TrackerRouter (/api/item) with better error handling

--- app/middleware/TrackerRouter.ts
+++ app/middleware/TrackerRouter.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { TrackerDataService } from '../services/TrackerDataService';
 import { IServiceRouter } from './IServiceRouter';
 
@@ -7,30 +7,34 @@ export class TrackerRouter implements IServiceRouter {
     router = Router();
 
     public constructor() {
-        this.router.get('/api/item', (request, response) => { this.apiGetItem(request, response); });
+        this.router.get('/', (request, response) => { this.pageOverview(request, response); });
+        this.router.get('/api/item', (request, response, next) => { (<any>request).is_api = true; this.apiGetItem(request, response, next); });
         //this.router.put('/api/item/:item', (request, response) => { this.apiAddItem(request, response); });
     }
 
-    protected apiGetItem(request: Request, response: Response) {
-        try {
-            let filter = { };
-            if (request.param('id') != null) {
-                filter['item_id'] = parseInt(request.param('id'));
-            } else if (request.param('search') != null) {
-                filter['name'] = { $regex: request.param('search') };
-            }
-
-            this.service.getItem(filter).then((docs) => {
-                response.send(docs);
-            }).catch((err) => {
-                response.send({ error: 'unknown error' });    
-            })  
-        } catch (err) {
+    protected apiGetItem(request: Request, response: Response, next: NextFunction) {
+        // get filter
+        let filter = { };
+        if (request.query['id'] != null && parseInt(request.query['id']) != NaN ) {
+            filter['item_id'] = parseInt(request.query['id']);
+        } else if (request.query['search'] != null) {
+            filter['name'] = { $regex: request.query['search'] };
+        } else {
             response.send({ error: 'bad arguments' });
         }
+        
+        this.service.getItem(filter).then((docs) => {
+            response.send(docs);
+        }).catch((err) => {
+            next(err);  
+        })
     }
 
     // protected apiAddItem(request: Request, response: Response) {
 
     // }
+
+    protected pageOverview(request, response) {
+        response.render('pages/tracker/overview', { });
+    }
 }
