8da8241e9d8bfe4a8a8d011caf18f529e6c38bee
DavidZhang309 <david_zhang_309@hotmail.com>
2017-05-10 16:07:25 -0400

    Added WIP code for authentication

--- app/index.ts
+++ app/index.ts
@@ -9,7 +9,13 @@ import * as express from 'express';
 import * as exprhandlebars from 'express-handlebars';
 import * as config from './config';
 
+//temp
+import * as passport from 'passport';
+import * as passport_local from 'passport-local';
+let LocalStrategy = passport_local.Strategy;
+
 let app = express();
+// Template engine configuration
 app.engine('handlebars', exprhandlebars({
     defaultLayout: 'main',
     layoutsDir: './app/templates/layouts/',
@@ -27,13 +33,35 @@ app.engine('handlebars', exprhandlebars({
 app.set('views', './app/templates');
 app.set('view engine', 'handlebars');
 
+// routing authentication
+app.use(passport.initialize());
+passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'passw'
+    }, function(user, pass, done) {
+        done(null, false, { message: "Not possible to authenticate" });
+    }
+));
+app.get('/auth/login', (request, response) => {
+    response.render('pages/auth/login', { });
+});
+app.post('/auth/api/login', passport.authenticate('local', { 
+        failureRedirect: '/auth/login'
+    }), (request, response) => {
+        response.redirect('/');
+    }
+);
+
+// routing services
 Object.keys(config.services).forEach((path) => {
     app.use(path, config.services[path].router);
 })
 app.get('/test-app', function(request, response) {
     response.render('pages/test-app/home', { layout: 'angular_base' });
 })
+//static files
 app.use('/', express.static('./build/client'));
+// 404 handling
 app.use('/', function(request, response, next) {
     response.statusCode = 404;
     if ((<any>request).is_api) {
@@ -44,6 +72,7 @@ app.use('/', function(request, response, next) {
         response.render("pages/404");
     }
 });
+// Server error handler (500)
 app.use(function(error: Error, request: Request, response: Response, next) { //error
     response.statusCode = 500;
     if ((<any>request).is_api) {
